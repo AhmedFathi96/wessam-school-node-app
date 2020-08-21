cfed1eaa9e2c9acec150a9321e24c249ca50068c
Ahmed Ebeid <a.ebeid@hotmail.com>
2020-08-21 03:29:58 +0200

    new fn

--- controllers/connect.js
+++ controllers/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://tacc:271996Ahmed@cluster0-xtqop.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://tacc:271996Ahmed@cluster0-xtqop.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const url = 'mongodb://localhost:27017/<dbname>';
 
 const Connect = async ()=>{
     await mongoose.connect(url ,
